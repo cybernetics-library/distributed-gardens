2c1dadee6c713519875f41fb80d91236fa41fc95
Dan Taeyoung <provolot@gmail.com>
2018-07-28 20:50:29 -0400

    adding plumbing for the time being

---
+++ plumbing/garden/node_irrigation_test.js
@@ -0,0 +1,5 @@
+var Irrigation  = require('./Irrigation');
+var irrigation = new Irrigation();
+irrigation.init()
+  .then(() => { console.log(irrigation.addSampleEvent()); })
+  .then(() => { console.log(irrigation.getEvents()); })
