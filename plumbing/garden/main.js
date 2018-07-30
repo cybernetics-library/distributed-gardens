b62a1175bfde03d1df6a3abb5576394e5b17591b
Dan Taeyoung <provolot@gmail.com>
2018-07-30 13:08:41 -0400

    garden clone of meadow

--- plumbing/garden/main.js
+++ plumbing/garden/main.js
@@ -1,46 +1,18 @@
-//import $ from 'jquery';
-//import _ from 'lodash'
-//import * as THREE from 'three';
-//import Vue from 'vue'
+import Vue from 'vue/dist/vue.esm.js'
+import Garden from './Garden.vue'
 
-
-import Irrigation from '../Irrigation';
-
-
-
-/*
-class Garden {
-  constructor(config) {
-    this.irrigation = new Irrigation();
-    this.irrigation.init()
-      .then(() => { })
-
-    });
-  }
-
+var testdata = {
 }
+var mizdata = require('./miserables.json');
 
+var vueapp = new Vue({
+  el: '#app',
+  data: {
+    graphdata: mizdata,
+  },
+  components: {
+    Garden
+  }
+})
 
-
-var garden = new Garden({})
-*/
-
-var irrigation = new Irrigation();
-irrigation.init()
-//  .then(() => { console.log(irrigation.addSampleEvent()); })
-  .then(() => { console.log(irrigation.getEvents()); })
-
-
-
-
-
-
-
-
-
-
-// for console debugging
-
-//window.$ = $;
-//window.garden = garden;
-
+window.vueapp = vueapp;
