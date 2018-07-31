a18ec283afe256a77085574371e6f37a55621b8f
Dan Taeyoung <provolot@gmail.com>
2018-07-31 19:43:28 -0400

    ask to reboot

--- kiosk/pi_setup_RUN_ONCE.sh
+++ kiosk/pi_setup_RUN_ONCE.sh
@@ -63,4 +63,5 @@ echo "$1" >> kiosk_ids.txt
 echo "=== Success! $1 added to kiosk_ids."
 echo "Please git commit -am 'kiosk registration' and push to master so that we can register kiosk_ids!"
 echo "OR add manually on another computer"
+echo "THEN REBOOOOT"
 
