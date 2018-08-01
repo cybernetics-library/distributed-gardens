aa85079c37bf1d7f71df36f25fa105936bb67d04
Dan Taeyoung <you@example.com>
2018-08-01 12:31:50 -0400

    adding refresh display

---
+++ kiosk/refresh_display.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#this is idempotent, so run any number of times
+
+echo "=== killing chromium browser"
+
+killall chromium-browser
+
+DISPLAY=:0 chromium-browser --noerrdialogs --incognito --kiosk --use-fake-ui-for-media-stream --test-type https://garden.decentralizedweb.net/splash/
+
+export DISPLAY=:0
+xset s noblank
+xset s activate
