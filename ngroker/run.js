566684e3d70fafcd98928de9ced821ad59caa65b
Dan Taeyoung <provolot@gmail.com>
2018-08-01 00:31:08 -0400

    Adding ngroker

---
+++ ngroker/run.js
@@ -0,0 +1,16 @@
+var SlackWebhook = require('slack-webhook')
+ 
+var slack = new SlackWebhook('https://hooks.slack.com/services/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', {
+ defaults: {
+    username: 'KioskBot',
+    icon_emoji: ':robot_face:'
+  }
+})
+
+const ngrok = require('ngrok');
+(async function() {
+  const url = await ngrok.connect(443);
+  slack.send(url)
+})();
+
+
