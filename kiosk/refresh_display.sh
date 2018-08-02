57279a925c72c4a64375bebe61c9ebf2fe78aba6
Dan Taeyoung <you@example.com>
2018-08-02 13:18:05 -0400

    refresh

--- kiosk/refresh_display.sh
+++ kiosk/refresh_display.sh
@@ -5,7 +5,10 @@
 echo "=== killing chromium browser"
 
 killall chromium-browser
+killall python
 
+cd ~/github/distributed-gardens/viz
+npm run production&
 DISPLAY=:0 chromium-browser --noerrdialogs --incognito --kiosk --use-fake-ui-for-media-stream --test-type http://localhost:8000
 
 export DISPLAY=:0
