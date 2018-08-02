5f9dd5277d95daff1b988d6ac9b6b26a467e6f83
Dan Taeyoung <you@example.com>
2018-08-02 13:20:14 -0400

    refresh without npm

--- kiosk/refresh_display.sh
+++ kiosk/refresh_display.sh
@@ -7,8 +7,8 @@ echo "=== killing chromium browser"
 killall chromium-browser
 killall python
 
-cd ~/github/distributed-gardens/viz
-npm run production&
+cd ~/github/distributed-gardens/viz/build
+python -m SimpleHTTPServer&
 DISPLAY=:0 chromium-browser --noerrdialogs --incognito --kiosk --use-fake-ui-for-media-stream --test-type http://localhost:8000
 
 export DISPLAY=:0
