20c2139be163df4b2e7b7b8fac6e0984787f443f
Dan Taeyoung <you@example.com>
2018-08-02 13:17:11 -0400

    refresh display

--- kiosk/splash_display.sh
+++ kiosk/splash_display.sh
@@ -6,7 +6,7 @@ echo "=== killing chromium browser"
 
 killall chromium-browser
 
-DISPLAY=:0 chromium-browser --noerrdialogs --incognito --kiosk --use-fake-ui-for-media-stream --test-type http://localhost:8000
+DISPLAY=:0 chromium-browser --noerrdialogs --incognito --kiosk --use-fake-ui-for-media-stream --test-type https://garden.decentralizedweb.net/splash/
 
 export DISPLAY=:0
 xset s noblank
